c56bf0185da24334d91b5969b1bf5ebc45322c2c
Sam3992 <samjoyemail@gmail.com>
2026-07-08 01:28:05 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,10 +7,12 @@ import (
 func main() {
 	var command string
 
-	fmt.Print("$ ")
-	_, err := fmt.Scanln(&command)
-	if err != nil {
-		fmt.Println("Failed to read input")
+	for {
+		fmt.Print("$ ")
+		_, err := fmt.Scanln(&command)
+		if err != nil {
+			fmt.Println("Failed to read input")
+		}
+		fmt.Printf("%s: command not found\n", command)
 	}
-	fmt.Printf("%s: command not found", command)
 }
