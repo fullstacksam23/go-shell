d4f66e24950617ad020082cf660d70e11e83315a
Sam3992 <samjoyemail@gmail.com>
2026-07-08 01:29:11 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -12,6 +12,10 @@ func main() {
 		_, err := fmt.Scanln(&command)
 		if err != nil {
 			fmt.Println("Failed to read input")
+			return
+		}
+		if command == "exit" {
+			return
 		}
 		fmt.Printf("%s: command not found\n", command)
 	}
