4420ff01467d91abb56ed39504e76ad4b1a57dc1
Sam3992 <samjoyemail@gmail.com>
2026-07-11 00:19:06 +0530

    codecrafters submit [skip ci]

--- app/parser.go
+++ app/parser.go
@@ -5,16 +5,22 @@ import "strings"
 func parse(input string) []string {
 	var tokens []string
 	var current strings.Builder
-	var inSingleQuotes bool
+	var inSingleQuotes, inDoubleQuotes bool
 	for _, r := range input {
 		switch {
-		case r == '\'' && !inSingleQuotes:
+		case r == '"' && !inDoubleQuotes && !inSingleQuotes:
+			inDoubleQuotes = true
+
+		case r == '"' && inDoubleQuotes && !inSingleQuotes:
+			inDoubleQuotes = false
+
+		case r == '\'' && !inSingleQuotes && !inDoubleQuotes:
 			inSingleQuotes = true
 
-		case r == '\'' && inSingleQuotes:
+		case r == '\'' && inSingleQuotes && !inDoubleQuotes:
 			inSingleQuotes = false
 
-		case r == ' ' && !inSingleQuotes:
+		case r == ' ' && !inSingleQuotes && !inDoubleQuotes:
 			if current.Len() > 0 {
 				tokens = append(tokens, current.String())
 				current.Reset()
