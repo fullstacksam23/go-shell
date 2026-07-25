84f99b46e590a202de12f5b26b13128553b70fcd
Sam3992 <samjoyemail@gmail.com>
2026-07-25 13:32:37 +0530

    codecrafters submit [skip ci]

--- builtins/echo.go
+++ builtins/echo.go
@@ -2,9 +2,42 @@ package builtins
 
 import (
 	"fmt"
+	"os"
 	"strings"
 )
 
 func Echo(args []string) {
-	fmt.Println(strings.Join(args, " "))
+	redirect := -1
+	for i, v := range args {
+		if v == ">" || v == "1>" {
+			redirect = i
+			break
+		}
+	}
+	if redirect == -1 {
+		fmt.Println(strings.Join(args, " "))
+		return
+	}
+
+	if redirect+1 >= len(args) {
+		fmt.Println("echo: missing file operand")
+		return
+	}
+
+	output := strings.Join(args[:redirect], " ")
+	fileName := args[redirect+1]
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(output)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
 }
