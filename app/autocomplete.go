286c5ace0f978019d3dc4d9fe34bff4320ab4a5d
Sam3992 <samjoyemail@gmail.com>
2026-07-28 17:29:47 +0530

    codecrafters submit [skip ci]

--- app/autocomplete.go
+++ app/autocomplete.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
 
 	"github.com/chzyer/readline"
 	"github.com/codecrafters-io/shell-starter-go/builtins"
@@ -26,16 +29,56 @@ func SetupAutoComplete() {
 	}
 
 	for k := range builtins.BuiltIn {
-		node := currTrie.root
-		for _, r := range k {
-			if node.children[r] == nil {
-				node.children[r] = &TrieNode{
-					children: make(map[rune]*TrieNode),
+		insertTrie(k)
+	}
+	loadExecutables()
+}
+
+func insertTrie(elem string) {
+	node := currTrie.root
+	for _, r := range elem {
+		if node.children[r] == nil {
+			node.children[r] = &TrieNode{
+				children: make(map[rune]*TrieNode),
+			}
+		}
+		node = node.children[r]
+	}
+	node.isEnd = true
+}
+
+func loadExecutables() {
+	pathEnv := os.Getenv("PATH")
+
+	for _, dir := range filepath.SplitList(pathEnv) {
+		dirs, err := os.ReadDir(dir)
+		if err != nil {
+			continue
+		}
+
+		for _, entry := range dirs {
+			if entry.IsDir() {
+				continue
+			}
+
+			info, err := entry.Info()
+			if err != nil {
+				continue
+			}
+
+			// Ignore non-executables
+			if runtime.GOOS == "windows" {
+				if filepath.Ext(entry.Name()) != ".exe" {
+					continue
+				}
+			} else {
+				if info.Mode()&0111 == 0 {
+					continue
 				}
 			}
-			node = node.children[r]
+
+			insertTrie(entry.Name())
 		}
-		node.isEnd = true
 	}
 }
 
